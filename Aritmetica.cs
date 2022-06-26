5e5c1b802c64fd8b7c6333b6a3339aede5bef1af
ErickFPCruz <erick.cruz97@hotmail.com>
2022-06-26 02:48:58 -0300

    A jornada estáquase no fim...

--- Aritmetica.cs
+++ Aritmetica.cs
@@ -6,10 +6,10 @@ public class Aritmetica
 
         Console.WriteLine("\nSoma");
 
-        Console.Write($"1° número = ");
+        Console.Write($"1° parcela = ");
         nr1 = Convert.ToDouble(Console.ReadLine());
 
-        Console.Write($"2° número = ");
+        Console.Write($"2° parcela = ");
         nr2 = Convert.ToDouble(Console.ReadLine());
 
         resultado = nr1 + nr2;
@@ -18,17 +18,16 @@ public class Aritmetica
         Console.ReadKey();
         return;
     }
-
     public static void Diferenca()
     {
         double nr1, nr2, resultado;
 
         Console.WriteLine("Digite os valores: ");
 
-        Console.Write($"1° número = ");
+        Console.Write($"Minuendo = ");
         nr1 = Convert.ToDouble(Console.ReadLine());
 
-        Console.Write($"2° número = ");
+        Console.Write($"Subtraendo = ");
         nr2 = Convert.ToDouble(Console.ReadLine());
 
         resultado = nr1 - nr2;
@@ -37,7 +36,6 @@ public class Aritmetica
         Console.ReadKey();
         return;
     }
-
     public static void Produto()
     {
         double nr1, nr2, resultado;
@@ -45,10 +43,10 @@ public class Aritmetica
         Console.WriteLine("Digite os valores: ");
 
         nr1 = Convert.ToDouble(Console.ReadLine());
-        Console.WriteLine($"1° número = {nr1}");
+        Console.WriteLine($"1° fator = {nr1}");
 
         nr2 = Convert.ToDouble(Console.ReadLine());
-        Console.WriteLine($"2° número = {nr2}");
+        Console.WriteLine($"2° fator = {nr2}");
 
         resultado = nr1 * nr2;
         Console.WriteLine($"\nTotal = {resultado}");
@@ -56,17 +54,16 @@ public class Aritmetica
         Console.ReadKey();
         return;
     }
-
     public static void Quociente()
     {
         double nr1, nr2, resultado;
 
         Console.WriteLine("Digite os valores: ");
 
-        Console.WriteLine($"1° número = ");
+        Console.WriteLine($"Dividendo = ");
         nr1 = Convert.ToDouble(Console.ReadLine());
 
-        Console.WriteLine($"2° número = ");
+        Console.WriteLine($"Divisor = ");
         nr2 = Convert.ToDouble(Console.ReadLine());
 
         if (nr2 == 0)
@@ -83,7 +80,6 @@ public class Aritmetica
         Console.ReadKey();
         return;
     }
-
     public static void Potencia()
     {
         double nr1, nr2, resultado;
@@ -102,7 +98,6 @@ public class Aritmetica
         Console.ReadKey();
         return;
     }
-
     public static void Raiz()
     {
         double nr1, nr2, resultado;
@@ -117,7 +112,12 @@ public class Aritmetica
 
         if (nr2 == 0)
         {
-            Console.WriteLine("Não existe raiz 0 de um número... Tente novamente...");
+            Console.WriteLine("Não existe raiz nula de um número... Tente novamente...");
+        }
+
+        else if (nr1 < 0)
+        {
+            Console.WriteLine("Não existe base negativa em uma raiz... Tente novamente...");
         }
 
         else
@@ -129,7 +129,6 @@ public class Aritmetica
         Console.ReadKey();
         return;
     }
-
     public static void AreaCirculo()
     {
         double nr1, resultado;
@@ -137,55 +136,118 @@ public class Aritmetica
         Console.WriteLine("Digite os valores: ");
 
         Console.WriteLine($"Raio = ");
-        nr1 = Convert.ToDouble(Console.ReadLine());        
-    }
+        nr1 = Convert.ToDouble(Console.ReadLine());
 
+        if (nr1 <= 0)
+        {
+            Console.WriteLine("Um raio é sempre positivo! Tente novamente...");
+        }
+
+        else
+        {
+            resultado = Math.PI * Math.Pow(nr1, 2);
+            Console.WriteLine($"\nTotal = {resultado}");
+        }
+    }
     public static void ComprimentoCirculo()
     {
         double nr1, resultado;
 
         Console.WriteLine("Digite os valores: ");
 
-        Console.WriteLine($"Base = ");
+        Console.WriteLine($"Raio = ");
         nr1 = Convert.ToDouble(Console.ReadLine());
-    }
 
+        if (nr1 <= 0)
+        {
+            Console.WriteLine("Um raio é sempre positivo! Tente novamente...");
+        }
+
+        else
+        {
+            resultado = 2 * Math.PI * nr1;
+            Console.WriteLine($"\nTotal = {resultado}");
+        }
+    }
     public static void VolumeCilindro()
     {
         double nr1, nr2, resultado;
 
         Console.WriteLine("Digite os valores: ");
 
-        Console.WriteLine($"Base = ");
+        Console.WriteLine($"Raio = ");
         nr1 = Convert.ToDouble(Console.ReadLine());
 
-        Console.WriteLine($"Índice = ");
+        Console.WriteLine($"Altura = ");
         nr2 = Convert.ToDouble(Console.ReadLine());
-    }
 
+        if (nr1 <= 0)
+        {
+            Console.WriteLine("Um raio é sempre positivo! Tente novamente...");
+        }
+
+        else if (nr2 <= 0)
+        {
+            Console.WriteLine("Uma altura é sempre positiva! Tente novamente...");
+        }
+
+        else
+        {
+            resultado = Math.PI * Math.Pow(nr1, 2) * nr2;
+            Console.WriteLine($"\nTotal = {resultado}");
+        }
+    }
     public static void Log()
     {
         double nr1, nr2, resultado;
 
         Console.WriteLine("Digite os valores: ");
 
-        Console.WriteLine($"Base = ");
+        Console.WriteLine($"Logaritmando = ");
         nr1 = Convert.ToDouble(Console.ReadLine());
 
-        Console.WriteLine($"Índice = ");
+        Console.WriteLine($"Base Log. = ");
         nr2 = Convert.ToDouble(Console.ReadLine());
-    }
 
+        if (nr1 <= 0)
+        {
+            Console.WriteLine("O Logaritmando precisa ser positivo! Tente novamente...");
+        }
+
+        else if (nr2 <= 0)
+        {
+            Console.WriteLine("A base precisa ser positiva! Tente novamente...");
+        }
+
+        else if (nr2 == 1)
+        {
+            Console.WriteLine("A base não pode ser 1, se não gera incompatibilidade! Tente novamente...");
+        }
+
+        else
+        {
+            resultado = Math.Log(nr1, nr2);
+            Console.WriteLine($"\nTotal = {resultado}");
+        }
+    }
     public static void Ln()
     {
-        double nr1, nr2, resultado;
+        double nr1, resultado;
 
         Console.WriteLine("Digite os valores: ");
 
-        Console.WriteLine($"Base = ");
+        Console.WriteLine($"Logaritmando = ");
         nr1 = Convert.ToDouble(Console.ReadLine());
 
-        Console.WriteLine($"Índice = ");
-        nr2 = Convert.ToDouble(Console.ReadLine());
+        if (nr1 <= 0)
+        {
+            Console.WriteLine("Tente novamente...");
+        }
+
+        else
+        {
+            resultado = Math.Log(nr1, Math.E);
+            Console.WriteLine($"\nTotal = {resultado}");
+        }
     }
 }
